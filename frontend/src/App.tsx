8067d1d56afa12e41121ece6f5c28c8b1738353b
haseeb lateef <lateefhaseeb1@gmail.com>
2026-07-27 18:29:25 +0100

    wire up react router with login/register routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <AuthProvider>
-      <div>Fitsens</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
